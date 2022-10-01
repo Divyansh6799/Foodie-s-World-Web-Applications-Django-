8630a9a45db747ffe897688664dddc6f9b602846
Divyansh6799 <divyanshtrivedi0607@gmail.com>
2022-10-01 20:40:14 +0530

    commit

--- manage.py
+++ manage.py
@@ -2,7 +2,7 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
-
+STATIC_ROOT = "/var/www/example.com/static/"
 
 def main():
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
